955934c00e92831493b0d9756b51b446c4d810cb
Tim Givois <tim.givois.mendez@gmail.com>
2019-07-27 19:32:11 -0700

    Add standard linter

--- index.d.ts
+++ index.d.ts
@@ -1,4 +1,4 @@
-declare module '*.css' 
+declare module '*.css'
 declare module '*.scss'
 declare module '*.html'
 declare module '*.png'
--- src/App.tsx
+++ src/App.tsx
@@ -5,18 +5,17 @@ import photo from './assets/photo-1.jpg'
 import security from './assets/security.svg'
 import worldMap from './assets/world-map.svg'
 
-
 const App: React.StatelessComponent<{}> = () => (
-    <div className='container'>
-        <h1>Hello World, React!</h1>
-        <div>
-            <img src={photo} className='image' />
-        </div>
-        <div>
-            <img src={security} className='image' />
-            <img src={worldMap} className='image' />
-        </div>
+  <div className='container'>
+    <h1>Hello World, React!</h1>
+    <div>
+      <img src={photo} className='image' />
     </div>
+    <div>
+      <img src={security} className='image' />
+      <img src={worldMap} className='image' />
+    </div>
+  </div>
 )
 
-export default App;
+export default App
